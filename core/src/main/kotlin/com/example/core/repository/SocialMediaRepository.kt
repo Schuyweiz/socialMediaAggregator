34b42951a821471e396fbf596137f53e1d32a373
Schuyweiz <kirill.pique97@mail.ru>
2022-04-25 03:56:52 +0300

    moved orelsethrow operation 1 level higher to run on heroku

--- core/src/main/kotlin/com/example/core/repository/SocialMediaRepository.kt
+++ core/src/main/kotlin/com/example/core/repository/SocialMediaRepository.kt
@@ -8,7 +8,7 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface SocialMediaRepository : JpaRepository<SocialMedia, Long>, CrudRepository<SocialMedia, Long> {
+interface SocialMediaRepository : JpaRepository<SocialMedia, Long>   {
 
     fun findByNativeIdAndSocialMediaType(nativeId: Long, type: SocialMediaType): SocialMedia?
 
--- core/src/main/kotlin/com/example/core/service/impl/SocialMediaQueryService.kt
+++ core/src/main/kotlin/com/example/core/service/impl/SocialMediaQueryService.kt
@@ -2,6 +2,8 @@ package com.example.core.service.impl
 
 import com.example.core.model.SocialMedia
 import com.example.core.repository.SocialMediaRepository
+import net.bytebuddy.implementation.bytecode.Throw
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
@@ -9,5 +11,5 @@ class SocialMediaQueryService(
     private val socialMediaRepository: SocialMediaRepository,
 ) {
 
-    fun findByIdOrThrow(id: Long): SocialMedia = socialMediaRepository.findById(id).orElseThrow()
+    fun findByIdOrThrow(id: Long): SocialMedia = socialMediaRepository.findByIdOrNull(id)?: throw Exception("")
 }
