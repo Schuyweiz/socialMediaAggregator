99164fe67648c080fe2cfdf9284457a5f66e11a3
Schuyweiz <kirill.pique97@mail.ru>
2022-04-30 19:08:52 +0300

    editiing handle to get facebook webhooks

--- api/src/main/kotlin/com/example/api/controller/FacebookWebhooksController.kt
+++ api/src/main/kotlin/com/example/api/controller/FacebookWebhooksController.kt
@@ -2,8 +2,10 @@ package com.example.api.controller
 
 import com.example.core.annotation.Logger
 import com.example.core.annotation.Logger.Companion.log
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -16,5 +18,13 @@ class FacebookWebhooksController {
         @RequestBody eventContent: String
     ) {
         log.info(eventContent)
+    }
+
+    @GetMapping("facebook/webhook/page")
+    fun verifyPageWebHook(
+        @RequestParam(name = "hub.challenge") verification: String
+    ): String {
+        log.info(verification)
+        return verification
     }
 }
